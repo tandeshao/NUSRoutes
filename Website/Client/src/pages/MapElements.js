a7a7567837291ce0b87de3d8d6119aaceb219e63
Tan De Shao <e0544387@u.nus.edu>
2021-07-02 00:59:58 +0800

    v1

--- Website/Client/src/pages/MapElements.js
+++ Website/Client/src/pages/MapElements.js
@@ -8,44 +8,44 @@ export const PageContainer = styled.div`
 `;
 
 export const SideBarContainer = styled.div`
-  width: ${({ sideBar }) => (sideBar ? "30vw" : "0vw")};
+  width: ${({ $sidebar }) => ($sidebar ? "30vw" : "0vw")};
   height: 100vh;
   z-index: 1;
   transition: all 0.15s ease-in-out;
   position: relative;
 
   @media screen and (max-width: 1500px) {
-    width: ${({ sideBar }) => (sideBar ? "50vw" : "0vw")};
+    width: ${({ $sidebar }) => ($sidebar ? "50vw" : "0vw")};
   }
 
   @media screen and (max-width: 1100px) {
-    width: ${({ sideBar }) => (sideBar ? "70vw" : "0vw")};
+    width: ${({ $sidebar }) => ($sidebar ? "70vw" : "0vw")};
   }
 
   @media screen and (max-width: 828px) {
-    width: ${({ sideBar }) => (sideBar ? "100vw" : "0vw")};
-    height: ${({ sideBar }) => (sideBar ? "100vh" : "0vh")};
+    width: ${({ $sidebar }) => ($sidebar ? "100vw" : "0vw")};
+    height: ${({ $sidebar }) => ($sidebar ? "100vh" : "0vh")};
   }
 
   @media screen and (max-height: 850px) {
-    width: ${({ sideBar }) => (sideBar ? "100vw" : "0vw")};
-    height: ${({ sideBar }) => (sideBar ? "100vh" : "0vh")};
+    width: ${({ $sidebar }) => ($sidebar ? "100vw" : "0vw")};
+    height: ${({ $sidebar }) => ($sidebar ? "100vh" : "0vh")};
   }
 `;
 
 export const MapContainer = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   z-index: 1;
 
   @media screen and (max-width: 828px) {
-    width: ${({ sideBar }) => (sideBar ? "0vw" : "100vw")};
-    height: ${({ sideBar }) => (sideBar ? "0vh" : "100vh")};
+    width: ${({ $sidebar }) => ($sidebar ? "0vw" : "100vw")};
+    height: ${({ $sidebar }) => ($sidebar ? "0vh" : "100vh")};
   }
 
   @media screen and (max-height: 850px) {
-    width: ${({ sideBar }) => (sideBar ? "0vw" : "100vw")};
-    height: ${({ sideBar }) => (sideBar ? "0vh" : "100vh")};
+    width: ${({ $sidebar }) => ($sidebar ? "0vw" : "100vw")};
+    height: ${({ $sidebar }) => ($sidebar ? "0vh" : "100vh")};
   }
 `;
 
@@ -56,35 +56,40 @@ export const ArrowLeftButton = styled(ArrowLeftRoundedIcon)`
     font-size: 100px;
     top: 50vh;
     color: black;
-    left: ${({ sideBar }) => (sideBar ? "23vw" : "0vw")};
+    left: ${({ $sidebar }) => ($sidebar ? "23vw" : "0vw")};
     cursor: pointer;
     background: #dfccb7;
     width: 1vw;
-    height: ${({ onHover }) => (onHover ? "6vh" : "4vh")};
+    height: 4vh;
     transition: background 2s;
+    
 
     @media screen and (max-width: 1500px) {
-      left: ${({ sideBar }) => (sideBar ? "33.2vw" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "33.2vw" : "0vw")};
     }
 
     @media screen and (max-width: 1100px) {
-      left: ${({ sideBar }) => (sideBar ? "41.2vw" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "41.2vw" : "0vw")};
       width: 3vw;
     }
 
     @media screen and (max-width: 828px) {
       top: 35%;
-      left: ${({ sideBar }) => (sideBar ? "97%" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "97%" : "0vw")};
     }
 
     @media screen and (max-height: 850px) {
       top: 35%;
-      left: ${({ sideBar }) => (sideBar ? "97%" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "97%" : "0vw")};
     }
 
     @media screen and (max-width: 492px) {
       width: 6vw;
-      left: ${({ sideBar }) => (sideBar ? "94%" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "94%" : "0vw")};
+    }
+
+    &:hover{
+      height: 6vh;
     }
   }
 `;
@@ -96,35 +101,39 @@ export const ArrowRightButton = styled(ArrowRightRoundedIcon)`
     font-size: 100px;
     top: 50vh;
     color: black;
-    left: ${({ sideBar }) => (sideBar ? "23vw" : "0vw")};
+    left: ${({ $sidebar }) => ($sidebar ? "23vw" : "0vw")};
     cursor: pointer;
     background: #dfccb7;
     width: 1vw;
-    height: ${({ onHover }) => (onHover ? "6vh" : "4vh")};
+    height: 4vh;
     transition: background 2s;
 
     @media screen and (max-width: 1500px) {
-      left: ${({ sideBar }) => (sideBar ? "33.2vw" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "33.2vw" : "0vw")};
     }
 
     @media screen and (max-width: 1100px) {
-      left: ${({ sideBar }) => (sideBar ? "41.2vw" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "41.2vw" : "0vw")};
       width: 3vw;
     }
 
     @media screen and (max-width: 828px) {
       top: 35%;
-      left: ${({ sideBar }) => (sideBar ? "97%" : "0%")};
+      left: ${({ $sidebar }) => ($sidebar ? "97%" : "0%")};
     }
 
     @media screen and (max-height: 850px) {
       top: 35%;
-      left: ${({ sideBar }) => (sideBar ? "97%" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "97%" : "0vw")};
     }
 
     @media screen and (max-width: 492px) {
       width: 6vw;
-      left: ${({ sideBar }) => (sideBar ? "94%" : "0vw")};
+      left: ${({ $sidebar }) => ($sidebar ? "94%" : "0vw")};
+    }
+
+    &:hover{
+      height: 6vh;
     }
   }
 `;
