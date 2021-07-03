48882a87d87c337cc1c781c903377edc4b4eb73c
Tan De Shao <e0544387@u.nus.edu>
2021-07-03 11:45:47 +0800

    FAQ and Info Section updated

--- Website/Client/src/components/FAQ/Data.js
+++ Website/Client/src/components/FAQ/Data.js
@@ -1,19 +1,43 @@
 export const Data = [
   {
-    question: "What is NUSRoute?",
+    question: "How do I use NUSRoutes?",
     answer:
       "Navigation App specifically for NUS...",
   },
   {
     question:
-      "How do I use NUSRoute?",
+      "What devices is NUSRoutes supported on?",
     answer:
       "Click...",
   },
   {
     question:
-      "How is NUSRoute better than NUS NextBus?",
+      "How is NUSRoutes better than the NUS NextBus App?",
     answer:
       "Faster loading",
   },
+  {
+    question:
+      "If I have encountered issues, who do I contact?",
+    answer:
+      "Faster loading",
+  },
+  {
+    question:
+      "Why does it take quite awhile for the webpage to load?",
+    answer:
+      "Faster loading",
+  },
+  {
+    question:
+      "What kind of features are available on NUSRoutes?",
+    answer:
+      "Faster loading",
+  },
+  {
+    question:
+      "Would it be possible to view the bus arrival timings without having to prompt the app for a route recommendation?",
+    answer:
+      "Faster loading",
+  }
 ];
