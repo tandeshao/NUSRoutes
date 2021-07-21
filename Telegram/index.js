2473225b6581bbd33fb10720360bf6cd20e7151d
atmh <alvin_tay98@hotmail.com>
2021-07-21 22:48:22 +0800

    Update teleBot

--- Telegram/index.js
+++ Telegram/index.js
@@ -17,58 +17,17 @@ var current = {};
 var destination = {};
 
 async function routeFinder(current, destination, ctx) {
-  // Current Time
-  let [hour, minute] = new Date().toLocaleTimeString("it-IT").split(/:| /);
-  let time = hour + minute;
-  const obj = new Date();
-  let [month, date, year] = obj.toLocaleDateString("en-US").split("/");
-  const day = obj.getDay();
-  
   // Get route
   const start = map[current];
   const end = map[destination];
-  const category =
-    data.reduce((x, y) => {
-      if (x !== null) {
-        return x;
-      } else if (y["duration-year"].includes(parseInt(year))) {
-        if (
-          y["duration-month"][0] < parseInt(month) &&
-          parseInt(month) < y["duration-month"][1]
-        ) {
-          return "vacation";
-        } else if (
-          y["duration-month"][0] === parseInt(month) ||
-          parseInt(month) === y["duration-month"][1]
-        ) {
-          return y["duration-date"][0] <= parseInt(date) &&
-            parseInt(date) <= y["duration-date"][1]
-            ? "vacation"
-            : "term";
-        } else {
-          return "term";
-        }
-      } else {
-        return null;
-      }
-    }, null) +
-    (parseInt(day) === 0
-      ? "-sun/ph"
-      : parseInt(day) === 6
-      ? "-sat"
-      : "-weekdays");
 
   try {
     const route = await fetch(
       "https://nusroutesapi.herokuapp.com" +
-        "/api/routeRecommedation?start=" +
+        "/api/telegramRouteRecommendation?start=" +
         encodeURIComponent(start) +
         "&end=" +
-        encodeURIComponent(end) +
-        "&time=" +
-        encodeURIComponent(time) +
-        "&date=" +
-        encodeURIComponent(category)
+        encodeURIComponent(end)
     ).then((response) => response.json());
 
     var reply = "";
@@ -114,24 +73,85 @@ function formatPath(arr) {
   return path;
 }
 
-async function arrivalTime(arrBusStop, busService, ctx) {
-  try {
-    const stop = map[arrBusStop];
-    const arrTime = await fetch(
-      "https://nusroutesapi.herokuapp.com" +
-        "/api/getArrivalTime?busStop=" +
-        encodeURIComponent(stop) +
-        "&busService=" +
-        encodeURIComponent(busService)
-    ).then((response) => response.json());
+async function arrivalTime(arrBusStop, busService, ctx, busstopcode) {
+  if (busstopcode) {
+    try {
+      const stop = map[arrBusStop];
+      const arrTime = await fetch(
+        "https://nusroutesapi.herokuapp.com" +
+          "/api/getArrivalTime?busStop=" +
+          encodeURIComponent(stop) +
+          "&busService=" +
+          encodeURIComponent(busService) +
+          "&busstopcode=" +
+          encodeURIComponent(busstopcode)
+      ).then((response) => response.json());
 
-    console.log(arrTime);
+      if ((arrTime[0] = "-")) {
+        arrTime[0] = "nil";
+      } else if ((arrTime[0] = "Arr")) {
+        arrTime[0] = "Arr";
+      } else {
+        arrTime[0] = arrTime[0] + " mins";
+      }
 
-    var reply = `Arriving: ${arrTime[0]}mins \nNext: ${arrTime[1]}mins`;
-    ctx.reply(reply);
-  } catch (error) {
-    console.log(error);
-    ctx.reply("Error occured. Server is down.");
+      if ((arrTime[1] = "-")) {
+        arrTime[1] = "nil";
+      } else if ((arrTime[0] = "Arr")) {
+        arrTime[1] = "Arr";
+      } else {
+        arrTime[1] = arrTime[0] + " mins";
+      }
+
+      var reply;
+      if (arrTime[0] === "nil" && arrTime[1] === "nil") {
+        reply = "No incoming bus";
+      } else {
+        reply = `Arriving: ${arrTime[0]} \nNext: ${arrTime[1]}`;
+      }
+      ctx.reply(reply);
+    } catch (error) {
+      console.log(error);
+      ctx.reply("Error occured. Server is down.");
+    }
+  } else {
+    try {
+      const stop = map[arrBusStop];
+      const arrTime = await fetch(
+        "https://nusroutesapi.herokuapp.com" +
+          "/api/getArrivalTime?busStop=" +
+          encodeURIComponent(stop) +
+          "&busService=" +
+          encodeURIComponent(busService)
+      ).then((response) => response.json());
+
+      if ((arrTime[0] = "-")) {
+        arrTime[0] = "nil";
+      } else if ((arrTime[0] = "Arr")) {
+        arrTime[0] = "Arr";
+      } else {
+        arrTime[0] = arrTime[0] + " mins";
+      }
+
+      if ((arrTime[1] = "-")) {
+        arrTime[1] = "nil";
+      } else if ((arrTime[0] = "Arr")) {
+        arrTime[1] = "Arr";
+      } else {
+        arrTime[1] = arrTime[0] + " mins";
+      }
+
+      var reply;
+      if (arrTime[0] === "nil" && arrTime[1] === "nil") {
+        reply = "No incoming bus";
+      } else {
+        reply = `Arriving: ${arrTime[0]} \nNext: ${arrTime[1]}`;
+      }
+      ctx.reply(reply);
+    } catch (error) {
+      console.log(error);
+      ctx.reply("Error occured. Server is down.");
+    }
   }
 }
 
@@ -352,6 +372,16 @@ bot.action("cur_The Japanese Primary School", (ctx) => {
 
 bot.action("cur_Kent Ridge Bus Terminal", (ctx) => {
   current[ctx.from.id] = "Kent Ridge Bus Terminal";
+  dest(ctx);
+});
+
+bot.action("cur_SDE 3", (ctx) => {
+  current[ctx.from.id] = "SDE 3";
+  dest(ctx);
+});
+
+bot.action("cur_Opp SDE 3", (ctx) => {
+  current[ctx.from.id] = "Opp SDE 3";
   dest(ctx);
 });
 
@@ -669,6 +699,24 @@ bot.action("dest_Kent Ridge Bus Terminal", (ctx) => {
   routeFinder(current[ctx.from.id], destination[ctx.from.id], ctx);
 });
 
+bot.action("dest_SDE 3", (ctx) => {
+  ctx.deleteMessage();
+  destination[ctx.from.id] = "SDE 3";
+  ctx.reply(
+    `Calculating... ${current[ctx.from.id]} to ${destination[ctx.from.id]}`
+  );
+  routeFinder(current[ctx.from.id], destination[ctx.from.id], ctx);
+});
+
+bot.action("dest_Opp SDE 3", (ctx) => {
+  ctx.deleteMessage();
+  destination[ctx.from.id] = "Opp SDE 3";
+  ctx.reply(
+    `Calculating... ${current[ctx.from.id]} to ${destination[ctx.from.id]}`
+  );
+  routeFinder(current[ctx.from.id], destination[ctx.from.id], ctx);
+});
+
 /* ----------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
@@ -707,6 +755,176 @@ bot.action("arr_AS5", (ctx) => {
   selectBusServices(ctx);
 });
 
+bot.action("arr_BIZ 2", (ctx) => {
+  arrBusStop[ctx.from.id] = "BIZ 2";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Botanic Gardens MRT", (ctx) => {
+  arrBusStop[ctx.from.id] = "Botanic Gardens MRT";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_BTC - Oei Tiong Ham Building", (ctx) => {
+  arrBusStop[ctx.from.id] = "BTC - Oei Tiong Ham Building";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Central Library", (ctx) => {
+  arrBusStop[ctx.from.id] = "Central Library";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_College Green", (ctx) => {
+  arrBusStop[ctx.from.id] = "College Green";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_COM2", (ctx) => {
+  arrBusStop[ctx.from.id] = "COM2";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_EA", (ctx) => {
+  arrBusStop[ctx.from.id] = "EA";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Information Technology", (ctx) => {
+  arrBusStop[ctx.from.id] = "Information Technology";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Kent Ridge MRT", (ctx) => {
+  arrBusStop[ctx.from.id] = "Kent Ridge MRT";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Kent Vale", (ctx) => {
+  arrBusStop[ctx.from.id] = "Kent Vale";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_LT13", (ctx) => {
+  arrBusStop[ctx.from.id] = "LT13";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_LT27", (ctx) => {
+  arrBusStop[ctx.from.id] = "LT27";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Museum", (ctx) => {
+  arrBusStop[ctx.from.id] = "Museum";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp HSSML", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp HSSML";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp Kent Ridge MRT", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp Kent Ridge MRT";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp NUSS", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp NUSS";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp TCOMS", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp TCOMS";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp University Hall", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp University Hall";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp University Health Centre", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp University Health Centre";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp YIH", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp YIH";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Prince George's Park", (ctx) => {
+  arrBusStop[ctx.from.id] = "Prince George's Park";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Prince George's Park Residence", (ctx) => {
+  arrBusStop[ctx.from.id] = "Prince George's Park Residence";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Raffles Hall (Opp. Museum)", (ctx) => {
+  arrBusStop[ctx.from.id] = "Raffles Hall (Opp. Museum)";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_S17", (ctx) => {
+  arrBusStop[ctx.from.id] = "S17";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_TCOMS", (ctx) => {
+  arrBusStop[ctx.from.id] = "TCOMS";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_University Hall", (ctx) => {
+  arrBusStop[ctx.from.id] = "University Hall";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_University Health Centre", (ctx) => {
+  arrBusStop[ctx.from.id] = "University Health Centre";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_University Town", (ctx) => {
+  arrBusStop[ctx.from.id] = "University Town";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Ventus (Opp LT13)", (ctx) => {
+  arrBusStop[ctx.from.id] = "Ventus (Opp LT13)";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_YIH", (ctx) => {
+  arrBusStop[ctx.from.id] = "YIH";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_The Japanese Primary School", (ctx) => {
+  arrBusStop[ctx.from.id] = "The Japanese Primary School";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Kent Ridge Bus Terminal", (ctx) => {
+  arrBusStop[ctx.from.id] = "Kent Ridge Bus Terminal";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_SDE 3", (ctx) => {
+  arrBusStop[ctx.from.id] = "SDE 3";
+  selectBusServices(ctx);
+});
+
+bot.action("arr_Opp SDE 3", (ctx) => {
+  arrBusStop[ctx.from.id] = "Opp SDE 3";
+  selectBusServices(ctx);
+});
+
 /* ----------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
@@ -716,6 +934,96 @@ bot.action("arr_AS5", (ctx) => {
 bot.action("service_A1", (ctx) => {
   ctx.deleteMessage();
   busService[ctx.from.id] = "A1";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_A2", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "A2";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_D1", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "D1";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_D1 (BIZ2)", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "D1";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} (BIZ2) at ${
+      arrBusStop[ctx.from.id]
+    }`
+  );
+  arrivalTime(
+    arrBusStop[ctx.from.id],
+    busService[ctx.from.id],
+    ctx,
+    "COM2-BIZ2"
+  );
+});
+
+bot.action("service_D1 (UTOWN)", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "D1";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} (UTOWN) at ${
+      arrBusStop[ctx.from.id]
+    }`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx, "COM2-UT");
+});
+
+bot.action("service_D2", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "D2";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_E", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "E";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_K", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "K";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_BTC", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "BTC";
+  ctx.reply(
+    `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
+  );
+  arrivalTime(arrBusStop[ctx.from.id], busService[ctx.from.id], ctx);
+});
+
+bot.action("service_L", (ctx) => {
+  ctx.deleteMessage();
+  busService[ctx.from.id] = "L";
   ctx.reply(
     `Arrival Time of ${busService[ctx.from.id]} at ${arrBusStop[ctx.from.id]}`
   );
@@ -806,6 +1114,176 @@ bot.on("edited_message", (ctx) => {
 
 bot.action("prox_AS5", (ctx) => {
   proxDest[ctx.from.id] = "AS5";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_BIZ 2", (ctx) => {
+  proxDest[ctx.from.id] = "BIZ 2";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Botanic Gardens MRT", (ctx) => {
+  proxDest[ctx.from.id] = "Botanic Gardens MRT";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_BTC - Oei Tiong Ham Building", (ctx) => {
+  proxDest[ctx.from.id] = "BTC - Oei Tiong Ham Building";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Central Library", (ctx) => {
+  proxDest[ctx.from.id] = "Central Library";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_College Green", (ctx) => {
+  proxDest[ctx.from.id] = "College Green";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_COM2", (ctx) => {
+  proxDest[ctx.from.id] = "COM2";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_EA", (ctx) => {
+  proxDest[ctx.from.id] = "EA";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Information Technology", (ctx) => {
+  proxDest[ctx.from.id] = "Information Technology";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Kent Ridge MRT", (ctx) => {
+  proxDest[ctx.from.id] = "Kent Ridge MRT";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Kent Vale", (ctx) => {
+  proxDest[ctx.from.id] = "Kent Vale";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_LT13", (ctx) => {
+  proxDest[ctx.from.id] = "LT13";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_LT27", (ctx) => {
+  proxDest[ctx.from.id] = "LT27";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Museum", (ctx) => {
+  proxDest[ctx.from.id] = "Museum";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp HSSML", (ctx) => {
+  proxDest[ctx.from.id] = "Opp HSSML";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp Kent Ridge MRT", (ctx) => {
+  proxDest[ctx.from.id] = "Opp Kent Ridge MRT";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp NUSS", (ctx) => {
+  proxDest[ctx.from.id] = "Opp NUSS";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp TCOMS", (ctx) => {
+  proxDest[ctx.from.id] = "Opp TCOMS";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp University Hall", (ctx) => {
+  proxDest[ctx.from.id] = "Opp University Hall";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp University Health Centre", (ctx) => {
+  proxDest[ctx.from.id] = "Opp University Health Centre";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp YIH", (ctx) => {
+  proxDest[ctx.from.id] = "Opp YIH";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Prince George's Park", (ctx) => {
+  proxDest[ctx.from.id] = "Prince George's Park";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Prince George's Park Residence", (ctx) => {
+  proxDest[ctx.from.id] = "Prince George's Park Residence";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Raffles Hall (Opp. Museum)", (ctx) => {
+  proxDest[ctx.from.id] = "Raffles Hall (Opp. Museum)";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_S17", (ctx) => {
+  proxDest[ctx.from.id] = "S17";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_TCOMS", (ctx) => {
+  proxDest[ctx.from.id] = "TCOMS";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_University Hall", (ctx) => {
+  proxDest[ctx.from.id] = "University Hall";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_University Health Centre", (ctx) => {
+  proxDest[ctx.from.id] = "University Health Centre";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_University Town", (ctx) => {
+  proxDest[ctx.from.id] = "University Town";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Ventus (Opp LT13)", (ctx) => {
+  proxDest[ctx.from.id] = "Ventus (Opp LT13)";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_YIH", (ctx) => {
+  proxDest[ctx.from.id] = "YIH";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_The Japanese Primary School", (ctx) => {
+  proxDest[ctx.from.id] = "The Japanese Primary School";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Kent Ridge Bus Terminal", (ctx) => {
+  proxDest[ctx.from.id] = "Kent Ridge Bus Terminal";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_SDE 3", (ctx) => {
+  proxDest[ctx.from.id] = "SDE 3";
+  ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
+});
+
+bot.action("prox_Opp SDE 3", (ctx) => {
+  proxDest[ctx.from.id] = "Opp SDE 3";
   ctx.reply(`Destination: ${proxDest[ctx.from.id]} ${locationStr}`);
 });
 
