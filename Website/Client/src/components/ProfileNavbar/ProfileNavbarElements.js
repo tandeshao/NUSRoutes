958b681800d29048e5fc7127a26db5f99b6ae0d1
Tan De Shao <e0544387@u.nus.edu>
2021-07-24 21:09:17 +0800

    changed profile navbar colour

--- Website/Client/src/components/ProfileNavbar/ProfileNavbarElements.js
+++ Website/Client/src/components/ProfileNavbar/ProfileNavbarElements.js
@@ -29,7 +29,7 @@ export const NavbarContainer = styled.div`
 `;
 
 export const NavLogo = styled(LinkR)`
-  color: #ff2400;
+  color: #fff;
   justify-self: flex-start;
   cursor: pointer;
   font-size: 1.5rem;
